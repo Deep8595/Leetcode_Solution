3fec9b6f47f621495869f491eb90f47f5fa8b3ba
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-01-21 11:01:12 +0530

    Time: 12 ms (72.99%), Space: 44.3 MB (93.67%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -6,6 +6,8 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
 
             arr[s.charAt(i) - 'a'] = i;
+
+            // System.out.println(arr[s.charAt(i) - 'a'] = i);
             
             if (arr[0] != -1 && arr[1] != -1 && arr[2] != -1) {
             count += 1 + Math.min( Math.min(arr[0], arr[1]) ,  arr[2]);
