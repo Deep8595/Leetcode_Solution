eeaae0548941bc93a937f9cc78ef6cd29437b044
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-01-27 11:46:06 +0530

    Time: 3 ms (46.71%), Space: 42.7 MB (72.32%) - LeetHub

---
+++ 0985-bag-of-tokens/0985-bag-of-tokens.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        int start = 0;
+        int end = tokens.length -1;
+        int score = 0;
+        int maxScore = 0;
+      
+        Arrays.sort(tokens);
+
+        while (start <= end) {
+           
+           if (tokens[start] <= power) {
+                power = power - tokens[start];
+             
+                score++;
+                start++;
+             maxScore = Math.max(score , maxScore);
+            }
+            else if( score > 0){
+                power = power + tokens[end];
+                score--;
+                end--;
+            }
+            else 
+            break;
+        }
+        return maxScore;
+    }
+}
