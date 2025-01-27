6ec751dd074adf7ff2bedd55cc7b299a422503c5
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-01-27 15:45:22 +0530

    Time: 9 ms (49.09%), Space: 46.7 MB (54.14%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-
         Arrays.sort(intervals, (interval1, interval2) -> interval1[0] - interval2[0]);
         int i = 0, j = 1;
         int end = intervals[0][1];
         List<int[]> answer = new ArrayList<>();
-      
         while (j < intervals.length && i < j) {
             if (intervals[j][0] <= end) {
                 end = Math.max(intervals[j][1], end);
