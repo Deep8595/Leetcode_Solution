a842f8c8153b1120fbe7f44b86920d46e8a4d704
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-01-29 23:12:43 +0530

    Time: 0 ms (100%), Space: 41.6 MB (65.43%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,18 +1,20 @@
 class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
-        int[] arr = new int[n + 1];
-        for (int citation : citations) {
-            if (citation > n) {
+        int arr[] = new int[n+1];
+        for(int i = 0 ; i < n ; i++){
+            if( citations[i] >= n){
                 arr[n]++;
-            } else {
-                arr[citation]++;
+            }
+            else{
+                arr[citations[i]]++;
             }
         }
+
         int total = 0;
-        for (int i = n; i >= 0; i--) {
+        for(int i = n ; i >= 0; i--){
             total += arr[i];
-            if (total >= i) {
+            if( total >= i){
                 return i;
             }
         }
