9045dec6f19e9267d90cfb9e2c02c7110e9872de
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-02-01 21:15:38 +0530

    Time: 3 ms (78.38%), Space: 56.7 MB (18.43%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -6,7 +6,8 @@ class Solution {
         maxAvg = sum / k;
         while( i < nums.length){
             sum += nums[i] - nums[i-k];
-            maxAvg = Math.max(maxAvg , sum/k);
+            // maxAvg = Math.max(maxAvg , sum/k);
+            if(sum/k > maxAvg) maxAvg = sum /k;
             i++;
         }
         return maxAvg;
