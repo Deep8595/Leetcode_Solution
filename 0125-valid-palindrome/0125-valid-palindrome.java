def44aa0584f635adf2ee360f745652af49ffb85
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-02-09 22:53:53 +0530

    Time: 3 ms (74.69%), Space: 43.7 MB (52.86%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length() == 0 || s == null ){
+            return false;
+        }
+        s = s.toLowerCase();
+
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0 ; i < s.length() ; i++){
+            char c = s.charAt(i);
+            if((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')){
+                ans.append(c);
+            }
+        }
+
+        
+        char[] charArray = ans.toString().toCharArray();
+
+        int start = 0 , end = ans.length() -1;
+
+        while( start < end){
+            if(charArray[start] != charArray[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
