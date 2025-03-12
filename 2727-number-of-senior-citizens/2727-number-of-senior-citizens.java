a9301bb317cc408dcfb06fbe92ed6e1163a85621
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-03-12 11:49:09 +0530

    Time: 1 ms (76.81%), Space: 42.7 MB (46.29%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -1,14 +1,13 @@
 class Solution {
     public int countSeniors(String[] details) {
-        int i = 0, sum = 0;
-        while( i < details.length){
-            String detail = details[i];
-            int age = Integer.parseInt(detail.substring(11,13));
-            if( age > 60){
-                sum++;
+        int count = 0;
+        for( int i = 0 ; i < details.length; i++){
+            String str = details[i];
+            int age = Integer.parseInt(str.substring(11,13));
+            if(age > 60){
+                count++;
             }
-            i++;
         }
-        return sum;
+        return count;
     }
 }
