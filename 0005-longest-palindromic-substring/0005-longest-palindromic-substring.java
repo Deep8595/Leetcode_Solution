e4ac1cb40c1080528352df4162918f0c12ccc7ce
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-03-28 12:00:58 +0530

    Time: 14 ms (92.89%), Space: 42.3 MB (74.88%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,25 +1,24 @@
 class Solution {
-    int longPalindrome = 0;
-    int maxLen = 0;
+    int LongPalind = 0;
+    int len = 0;
     public String longestPalindrome(String s) {
-        // a longPalindrome = a
-        if( s.length() < 2 ) return s; 
-        for( int i  = 0  ; i < s.length() -1 ; i++){
-            // odd madam
-            palindrome(s , i , i);
-            // even abba
+        if( s.length() < 2) return s;
+        for( int i = 0 ; i < s.length() -1 ; i++){
+            // odd 
+            palindrome( s , i , i);
+            // even 
             palindrome( s, i , i+1);
         }
-        return s.substring(longPalindrome ,  longPalindrome + maxLen);
+        return s.substring(LongPalind , len + LongPalind);
     }
-    void palindrome(String s , int start , int end){
-        while( start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
+    void palindrome(String str , int start , int end){
+        while( start >= 0 && end < str.length() && str.charAt(start) == str.charAt(end)){
             start--;
             end++;
         }
-        if( maxLen < (end - start -1)){
-            longPalindrome = start + 1;
-            maxLen = (end - start -1);
+        if( len < (end - start -1)){
+            LongPalind = start + 1;
+            len = (end - start -1);
         }
     }
 }
