c65cfbc18863f8756c5ae56050c0a9e0f0be4c96
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-04-01 23:53:57 +0530

    Time: 2 ms (12.44%), Space: 41.4 MB (66.51%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        char[] patternArr = pattern.toCharArray();
+        String[] strArr = s.split(" ");
+
+        if(strArr.length != patternArr.length) return false;
+
+        for( int i = 0 ; i < strArr.length; i++ ){
+            for(int j = i+1 ; j < strArr.length; j++){
+                if(patternArr[i] == patternArr[j] && !strArr[i].equals(strArr[j])){
+                    return false;
+                }
+                if(patternArr[i] != patternArr[j] && strArr[i].equals(strArr[j])){
+                    return false;
+                }
+            }
+        }
+
+         return true;
+    }
+}
