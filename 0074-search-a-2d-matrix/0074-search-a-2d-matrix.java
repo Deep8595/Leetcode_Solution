530d35cae18844115cb829b6ef5268eda5ebd934
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-04-13 14:58:03 +0530

    Time: 0 ms (100%), Space: 42.6 MB (18.39%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -20,13 +20,6 @@ class Solution {
         return false;
     }
     public boolean searchMatrix(int[][] matrix, int target) {
-        // for(int i = 0; i < matrix.length - 1; i ++){
-        //     for(int j = 0 ; j < matrix[0].length -1 ; j++){
-        //         if(matrix[i][j] == target){
-        //             return true;
-        //         }
-        //     }
-        // }
         boolean result = canFind(matrix , target);
         return result;
     }
