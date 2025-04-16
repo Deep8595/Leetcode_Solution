891e886e36eedf9570167be47b2e21130635c660
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-04-16 09:11:42 +0530

    Time: 144 ms (5.03%), Space: 44.8 MB (78.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,7 +4,6 @@ class Solution {
         for( int i = 0 ; i < nums.length ; i++){
             hash.put(nums[i] , i );
         }
-
         for( int i = 0 ; i < nums.length ; i++){
             int num = nums[i];
             int remain = target - num;
