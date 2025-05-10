5eea97f9872c5c8e3c473aea7763ebd03553bb00
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-05-10 11:01:42 +0530

    Time: 0 ms (100%), Space: 45.7 MB (56.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,4 +1,9 @@
 class Solution {
+    static {
+        for(int i=0;i<500;i++){
+            canJump(new int[]{0});
+        }
+    }
     public static boolean canJump(int[] nums) {
         int maxReach = 0;
         for(int i=0;i<nums.length;i++){
