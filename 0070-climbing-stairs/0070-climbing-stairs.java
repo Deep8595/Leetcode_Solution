31568ffde0ea2620184f86c71f0362ee5b2e532d
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-05-19 09:34:15 +0530

    Time: 0 ms (100%), Space: 40.5 MB (50.82%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        int [] arr = new int[n+1];
-        for( int i = 0 ; i <= n ; i++){
-            arr[i] = -1;
+        int [] arr = new int [n+1];
+        arr[0] = 1;
+        arr[1] = 1;
+        for( int i = 2 ; i <= n ; i++){
+            arr[i] = arr[i-1] + arr[i-2];
         }
-        return memo( n , arr );
-    }
-    private int memo(int n , int[] arr){
-        if( n <= 1 ) return 1;
-        if( arr[n] != -1 ) return arr[n];
-        arr[n] = memo( n -1 , arr ) + memo( n-2 , arr);
         return arr[n];
     }
 }
