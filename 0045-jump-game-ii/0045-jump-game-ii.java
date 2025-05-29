0300076891411703a7288a86e351673b6240c8c8
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-05-29 12:18:24 +0530

    Time: 1 ms (99.52%), Space: 44.6 MB (99.59%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,13 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
-        int reach = 0, ans = 0;
-        if( nums.length == 1) return ans;
-        for(int i=0 ; i < nums.length ; i++){
-            if( reach == i){
-                ans ++;
+        int jump = 0 , l = 0 , r = 0;
+        while( r < nums.length -1 ){
+            int far = 0;
+            for( int i = l; i <= r ; i++){
+                far = Math.max( i + nums[i] , far);
             }
-            reach = Math.max(reach , i+nums[i]);
+            l = r + 1;
+            r = far;
+            jump++;
         }
-        return  ans;
+        return jump;
     }
 }
