8b5dd8a1fbc9ada1dad17f0ad05b4f61746e6f30
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-06-21 16:09:59 +0530

    Time: 1 ms (99.84%), Space: 45.9 MB (32.08%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,15 +1,13 @@
 class Solution {
-    static {
-        for(int i=0;i<500;i++){
-            canJump(new int[]{0});
+    public boolean canJump(int[] nums) {
+        // Greedy 
+        // Arrays.sort(nums);
+        int n = nums.length-1;
+        for( int i = n - 1  ; i >= 0 ; i--){
+            if( i + nums[i] >= n ){
+                n = i;
+            } 
         }
-    }
-    public static boolean canJump(int[] nums) {
-        int maxReach = 0;
-        for(int i=0;i<nums.length;i++){
-            if(i>maxReach) return false;
-            maxReach = Math.max(maxReach, i+nums[i]);
-        }
-        return true;
+        return (n == 0);
     }
 }
