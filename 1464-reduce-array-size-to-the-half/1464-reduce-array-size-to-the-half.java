0c608f120a121fc39921b4bc27e5761ce0f600c4
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-06-27 11:19:30 +0530

    Time: 30 ms (88.94%), Space: 55.9 MB (94.3%) - LeetHub

--- 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.java
+++ 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.java
@@ -1,7 +1,6 @@
 class Solution {
     public int minSetSize(int[] arr) {
         Map<Integer , Integer> map = new HashMap<>();
-        Map<Integer , Integer> mapCon = new HashMap<>();
         int step = 0; 
         for( int i = 0 ; i < arr.length ; i++ ){
             if(map.containsKey(arr[i])){
