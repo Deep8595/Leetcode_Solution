484908a2139efe65df7992121f07cdd2e76e66ca
Deepanshu Singh Chauhan <128804139+Deep8595@users.noreply.github.com>
2025-06-27 11:19:11 +0530

    Time: 31 ms (88.48%), Space: 57 MB (87.78%) - LeetHub

---
+++ 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+        Map<Integer , Integer> map = new HashMap<>();
+        Map<Integer , Integer> mapCon = new HashMap<>();
+        int step = 0; 
+        for( int i = 0 ; i < arr.length ; i++ ){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i] , map.get(arr[i]) + 1);
+            }
+            else{
+                map.put(arr[i],1);
+            }
+        }
+        int[] array = new int[map.size()];
+        int index = 0;
+        for(Map.Entry<Integer , Integer> entry : map.entrySet()){
+            array[index++] = entry.getValue();
+        }
+        // array[i] = { 2, 4, 3, 1}
+        Arrays.sort(array);
+        int tar = arr.length / 2;
+        int sum = 0;
+        for(int i = array.length -1 ; i >= 0 ; i-- ){
+            sum += array[i];
+            step++;
+            if(sum >= tar) break;
+        }
+        return (step >= 1) ? step : 0;
+    }
+}
