14b6c9e7a272e8119a147a92ea691faabaec2555
Deepanshu Singh <128804139+Deep8595@users.noreply.github.com>
2025-07-03 12:07:48 +0530

    Time: 13 ms (61.01%), Space: 55.2 MB (58.64%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,30 +1,19 @@
 class Solution {
-    private static int canFind(int[] nums, int k) {
-
-        int start = 0;
-        int end = 0;
-        int count = 0;
-
-        while (end < nums.length) {
-            if (nums[end] % 2 == 1) {
-                k--;
-                while (k < 0) {
-                    if (nums[start] % 2 == 1) {
-                        k++;
-                    }
-                    start++;
-                }
-            }
-            count += end - start + 1;
-            end++;
-        }
-        return count;
-    }
-
     public int numberOfSubarrays(int[] nums, int k) {
-        int count1 = canFind(nums, k);
-        int count2 = canFind(nums, k - 1);
-        int diff = count1 - count2;
-        return diff;
+        return check(nums , k ) - check (nums , k -1);
+    }
+    private int check (int nums[] , int k ){
+        int i = 0 , res = 0 ;
+        for(int j = 0 ; j < nums.length ; j++){
+            if(nums[j] % 2 == 1) k--;
+            while( k < 0){
+                if(nums[i] % 2 == 1){
+                    k++;
+                }
+                i++;
+            }
+            res += j - i + 1;
+        }
+        return res;
     }
 }
