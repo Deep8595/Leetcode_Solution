b00a85b823d4dc0c4f95b736521bfffba93f46e5
Deepanshu Singh <128804139+Deep8595@users.noreply.github.com>
2025-07-04 15:10:00 +0530

    Time: 23 ms (47.25%), Space: 45.7 MB (63.86%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
@@ -1,38 +1,38 @@
-class Job {
-    int difficulty;
-    int profit;
-
-    Job(int difficulty, int profit) {
-        this.difficulty = difficulty;
-        this.profit = profit;
-    }
-}
-
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        Arrays.sort(worker);
         int n = difficulty.length;
-        Job jobs[] = new Job[n];
+        int[][] jobs = new int[n][2];
+
         for (int i = 0; i < n; i++) {
-            jobs[i] = new Job(difficulty[i], profit[i]);
+            jobs[i][0] = difficulty[i];
+            jobs[i][1] = profit[i];
         }
-        Arrays.sort(jobs, new Comparator<Job>() {
-            public int compare(Job j1, Job j2) {
-                return j1.difficulty - j2.difficulty;
+
+        int ans = 0;
+        Arrays.sort(worker);
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+
+        int[] count = new int[jobs[jobs.length - 1][0] + 1];
+        int m = count.length, j = 0;
+        count[0] = 0;
+        int best = 0;
+
+        for (int i = 1; i < m; i++) {
+            while (j < jobs.length && jobs[j][0] == i) {
+                best = Math.max(best, jobs[j][1]);
+                j++;
             }
-        });
-        int i = 0;
-        int maxProfit = 0;
-        PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Collections.reverseOrder());
-        for (int j = 0; j < worker.length; j++) {
-            while (i < n && jobs[i].difficulty <= worker[j]) {
-                maxPQ.offer(jobs[i].profit);
-                i++;
-            }
-            if (!maxPQ.isEmpty()) {
-                maxProfit += maxPQ.peek();
+            count[i] = Math.max(count[i - 1], best);
+        }
+
+        for (int w = 0; w < worker.length; w++) {
+            if (worker[w] < m) {
+                ans += count[worker[w]];
+            } else {
+                ans += count[m - 1];
             }
         }
-        return maxProfit;
+
+        return ans;
     }
 }
