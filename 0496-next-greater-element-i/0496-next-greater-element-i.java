ca6c9890fa0dbbbe1a97dc40029da79cfc75a25d
Deepanshu Singh <128804139+Deep8595@users.noreply.github.com>
2025-08-07 22:11:54 +0530

    Time: 6 ms (9.74%), Space: 43.8 MB (61.45%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -3,34 +3,32 @@ class Solution {
         int[] ans = new int[nums2.length];
         int[] answer = new int[nums1.length];
         Stack<Integer> stack = new Stack<>();
-        for(int i = nums2.length -1 ; i >= 0 ; i--){
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
             int el = nums2[i];
-            if(stack.isEmpty()){
-                stack.push(el);
-                ans[i] = -1;
-            }
-            if(stack.peek() > el){
-                ans[i] = stack.peek();
-                stack.push(el);
-            }
-            while( !stack.isEmpty() && stack.peek() <= el){
+
+            while (!stack.isEmpty() && stack.peek() <= el) {
                 stack.pop();
             }
-            if(stack.isEmpty()){
+
+            if (stack.isEmpty()) {
                 ans[i] = -1;
-            }else{
+            } else {
                 ans[i] = stack.peek();
             }
+
             stack.push(el);
         }
+
         int j = 0;
-        for(int i = 0 ; i < ans.length; i++){
-            if( j < nums1.length && nums2[i] == nums1[j]){
+        for (int i = 0; i < nums2.length; i++) {
+            if (j < nums1.length && nums2[i] == nums1[j]) {
                 answer[j] = ans[i];
                 j++;
                 i = -1;
             }
         }
+
         return answer;
     }
 }
