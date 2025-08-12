1d522134d6319bdb8bc18a2e6dd4fd5960c7d515
Deepanshu Singh <128804139+Deep8595@users.noreply.github.com>
2025-08-12 20:20:39 +0530

    Time: 1 ms (60.66%), Space: 41.5 MB (76.53%) - LeetHub

--- 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-
         String ans1 = concate(word1);
         String ans2 = concate(word2);
         return ans1.equals(ans2);
