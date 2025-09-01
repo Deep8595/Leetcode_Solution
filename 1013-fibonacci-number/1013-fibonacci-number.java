4ee59fca5fd4732d58349202a3310ac2d6a1ba5a
Deepanshu Singh <128804139+Deep8595@users.noreply.github.com>
2025-09-01 21:23:26 +0530

    Time: 0 ms (100%), Space: 40.3 MB (64.24%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,15 +1,15 @@
 class Solution {
     public int fib(int n) {
-        int arr[] = new int[n+1];
-        for(int i = 0; i <= n ; i++){
+        int[] arr = new int[n+1];
+        for(int i = 0 ; i < arr.length ; i++){
             arr[i] = -1;
         }
         return check( n , arr);
     }
-    private int check( int n , int[] arr){
-        if( n <= 1 ) return n;
-        if( arr[n] != -1 ) return arr[n];
-        arr[n] = check(n-1, arr) + check(n-2,arr);
+    public int check(int n , int[] arr){
+        if( n <= 1) return n;
+        if(arr[n] != -1) return arr[n];
+        arr[n] = check(n-1, arr) + check(n-2 , arr);
         return arr[n];
     }
 }
