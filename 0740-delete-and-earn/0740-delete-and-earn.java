e99054460de2295e5957f7d1a9c37dfc205d16e7
Deepanshu Singh <128804139+Deep8595@users.noreply.github.com>
2025-09-06 10:54:33 +0530

    Time: 5 ms (47.62%), Space: 46 MB (7.84%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int sum [] = new int[10001];
+        for(int num : nums){
+            sum[num] += num;
+        }
+        int dp [] = new int[10001];
+        dp[1] = sum[1];
+        for(int i = 2 ; i < 10001 ; i++){
+            dp[i] = Math.max(dp[i-2] + sum[i] , dp[i-1]);
+        }
+        return dp[10000];
+    }
+}
